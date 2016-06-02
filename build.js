1af81459192ee164018331873bac2aaf507e87d7
Alec Lomas <alec@lowmess.com>
2016-06-01 22:00:17 -0700

    Prose config AGAIN
    ¯\_(ツ)_/¯

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ var sass = require('metalsmith-sass')
 var prefix = require('metalsmith-autoprefixer')
 var bourbon = require('node-bourbon').includePaths
 
-Metalsmith(__dirname)
+var siteBuild = Metalsmith(__dirname)
   .source('source')
   .destination('_build')
   .metadata({
@@ -27,17 +27,17 @@ Metalsmith(__dirname)
       email: 'neo@nebuchadnezzar.ship',
       phone: '(312) 555-0690',
       location: '???'
-    },
-    social: {
-      LinkedIn: '',
-      GitHub: 'neo',
-      Twitter: '@neo',
-      Facebook: '',
-      Codepen: '',
-      Dribbble: '',
-      Tumblr: '',
-      reddit: '',
-      Medium: ''
+      social: {
+        LinkedIn: '',
+        GitHub: 'github.com/neo',
+        Twitter: 'twitter.com/neo',
+        Facebook: '',
+        Codepen: '',
+        Dribbble: '',
+        Tumblr: '',
+        reddit: '',
+        Medium: ''
+      }
     },
     proficiencies: [
       'martial arts',
@@ -81,12 +81,17 @@ Metalsmith(__dirname)
   }))
   .use(layouts({
     engine: 'pug',
-    moment: require('moment'),
     pretty: true,
+    moment: require('moment'),
     directory: 'templates',
     default: 'default.pug',
     pattern: '**/*.html'
   }))
-  .build(function (err) {
-    if (err) throw err
-  })
+
+siteBuild.build(function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Site build complete!')
+  }
+})
