b4b26a6a54c53d17a170e7595280c0f0715bb40e
Alec Lomas <alec@lowmess.com>
2016-07-21 01:20:45 -0700

    Trims uneccessary file path characters from PDF script

--- pdf.js
+++ pdf.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 var pdf = require('html-pdf')
-var html = fs.readFileSync('./_build/index.html', 'utf8')
+var html = fs.readFileSync('_build/index.html', 'utf8')
 var options = {
     height: '11in',
     width: '8.5in',
@@ -11,13 +11,13 @@ var options = {
 var server = require('browser-sync').create()
 
 server.init({
-  server: './_build/',
+  server: '_build',
   port: 8008,
   open: false,
   ui: false
 })
 
-pdf.create(html, options).toFile('./resume.pdf', function(err, res) {
+pdf.create(html, options).toFile('resume.pdf', function(err, res) {
   if (err) return console.log(err)
   console.log('PDF generation complete!\n' + res)
   server.exit()
